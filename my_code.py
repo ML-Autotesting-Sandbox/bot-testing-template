d171cca33959de0dbe44a55c255b02a0381cda27
Megan Leich <51799170+maleich@users.noreply.github.com>
2021-06-23 11:49:00 -0700

    Initial commit

---
+++ my_code.py
@@ -0,0 +1,8 @@
+#######################################
+# Collaborators (including web sites where you got help: 
+# (enter none if you didn't need help)
+#  
+#
+#######################################
+
+# print("This is a test of GitHub and PyCharm. It is only a test.")
